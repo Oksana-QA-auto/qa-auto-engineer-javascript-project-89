bde7e8508ff38b3d4bdbfd03b801ee1b4c19adc5
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-08-14 16:26:43 +0500

    Connect Flowbot widget (@hexlet/chatbot-v2) via example steps

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Widget from '@hexlet/chatbot-v2'
+import steps from '@hexlet/chatbot-v2/example-steps'
+import '@hexlet/chatbot-v2/styles'
+
+const rootEl = document.getElementById('root')
+
+createRoot(rootEl).render(
+  <React.StrictMode>
+    {Widget(steps)}
+  </React.StrictMode>
+)
