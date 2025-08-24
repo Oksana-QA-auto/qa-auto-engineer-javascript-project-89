b3852704a475ecf4f7cde38ef88ec27affdf5052
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-08-24 14:14:52 +0500

    Refactor(tests): move UI texts to constants and use them in widget.render test

---
+++ __tests__/helpers/texts.js
@@ -0,0 +1,7 @@
+export const startButtonText = 'открыть чат'
+
+export const modalTitleText = 'Виртуальный помощник'
+
+export const closeButtonAria = 'close'
+
+ 
--- __tests__/widget.render.test.jsx
+++ __tests__/widget.render.test.jsx
@@ -5,11 +5,12 @@ import userEvent from '@testing-library/user-event'
 import Widget from '@hexlet/chatbot-v2'
 import steps from '../__fixtures__/basic-steps.js'
 import { toRegex } from './helpers/test-utils.js'
+import { startButtonText } from './helpers/texts.js'
 
 test('виджет рендерится без ошибок', async () => {
   render(Widget(steps))
 
-  const openBtn = await screen.findByRole('button', { name: /открыть чат/i })
+  const openBtn = await screen.findByRole('button', { name: toRegex(startButtonText) })
   expect(openBtn).toBeInTheDocument()
 
   await userEvent.click(openBtn)
