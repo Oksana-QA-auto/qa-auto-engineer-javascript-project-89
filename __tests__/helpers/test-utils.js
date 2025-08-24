4f50bb7f6e69b4e6d4bdb122c97fefff6ad6b335
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-08-24 14:35:27 +0500

    Helpers: keep only used exports; make escapeForRegExp internal

--- __tests__/helpers/test-utils.js
+++ __tests__/helpers/test-utils.js
@@ -1,17 +1,18 @@
 // Общие утилиты для тестов
-export const deepClone = value => JSON.parse(JSON.stringify(value))
+export const deepClone = (value) => JSON.parse(JSON.stringify(value))
 
-export const escapeForRegExp = text =>
+const escapeForRegExp = (text) =>
   text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
 export const assertArray = (value, label = 'value') => {
   if (!Array.isArray(value)) {
     throw new Error(
-      `${label} is not an array (got ${typeof value}).
-      * Проверь импорт '../__fixtures__/basic-steps.js'
-      * и что экспортируется default [...] (массив шагов)`,
+      `\`${label}\` is not an array (got ${typeof value}).\n`
+      + '* Проверь импорт \'../__fixtures__/basic-steps.js\'\n'
+      + '* и что экспортируется default [...] (массив шагов)\n',
     )
   }
 }
 
-export const toRegex = text => new RegExp(escapeForRegExp(text), 'i')
+export const toRegex = (text) => new RegExp(escapeForRegExp(text), 'i')
+
