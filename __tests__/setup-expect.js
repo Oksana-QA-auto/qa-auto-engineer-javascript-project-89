02ac5cc611ec6190b56c8ecfdb99301a451a2587
Oksana Auto-QA <oksana.zhornova@gmail.com>
2026-01-14 15:33:11 +0500

    Fix jest-dom matchers setup for Vitest

--- __tests__/setup-expect.js
+++ __tests__/setup-expect.js
@@ -1,4 +1,4 @@
-import * as matchers from '@testing-library/jest-dom/matchers'
+import * as jestDomMatchers from '@testing-library/jest-dom/matchers'
 import { expect } from 'vitest'
 
-expect.extend(matchers)
+expect.extend(jestDomMatchers.default ?? jestDomMatchers)
