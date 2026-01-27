ebea0deffc77347128256b4fec3dfa884bdc114e
Oksana Auto-QA <oksana.zhornova@gmail.com>
2026-01-27 20:20:23 +0500

    Fix vitest setup

--- vitest.config.js
+++ vitest.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   },
 
   test: {
-    setupFiles: ['./vitest.setup.js', './__tests__/setup-expect.js'],
+    setupFiles: ['./vitest.setup.js'],
     environment: 'jsdom',
     globals: true,
 
